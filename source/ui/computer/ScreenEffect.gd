152873f78b093117075d9d135bcf5232964daea4
Luke Shiner <luke@lukeshiner.com>
2021-07-12 11:02:47 +0100

    Add screen flicker

---
+++ source/ui/computer/ScreenEffect.gd
@@ -0,0 +1,18 @@
+extends ColorRect
+
+var noise = OpenSimplexNoise.new()
+var initial_darkening = material.get_shader_param("amount")
+export(float) var speed = 50.0
+export(float) var darkning_varience = 0.25
+
+func _ready():
+	noise.seed = randi()
+	noise.period = 20.0
+	noise.persistence = 0.8
+
+func _process(delta):
+	var noise_amount = noise.get_noise_1d(OS.get_ticks_msec()/ speed)
+	var offset = lerp(-darkning_varience, darkning_varience, noise_amount)
+	var darkening = initial_darkening + offset
+	material.set_shader_param("amount", darkening)
+
